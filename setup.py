c5ab2a9dcdf24b3ff7166108c7d8e51ce5762708
Verinikita <80643476+Verinikita@users.noreply.github.com>
2024-01-29 15:00:43 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,28 @@
+import setuptools
 
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='RAMACHANDRAN',
+    version='0.2.4',
+    author='VB',
+    author_email='VB@gmail.com',
+    description='Ramachandran plotting tool',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/alxdrcirilo/RamachanDraw',
+    packages=['RAMACHANDRAN'],
+    package_data={'RAMACHANDRAN': ['./density_estimate.dat']},
+    install_requires=[
+        'biopython>=1.75',
+        'matplotlib>=3.1.2',
+        'rich>=9.3'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
